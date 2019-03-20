950bbf1b184891212094e3cdeadabf38cb517956
Castrucci <maximiliano.castrucci@igt.com>
2019-03-20 08:06:00 -0300

    updating DB info

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require ('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-metric-94863',
         user: 'postgres',
         password: 'brigida',
         database: 'smart-brain'
